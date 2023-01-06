bc851b323e64d98d2225af008089627472581d70
almacenero <yasser@dreamcode.io>
2023-01-06 15:34:37 -0600

    Added pagination to list posts

--- src/resolvers/Queries.ts
+++ src/resolvers/Queries.ts
@@ -2,12 +2,19 @@ import {Post} from "./../models/Post"
 import { Comment } from "../models"
 import { PostItem } from "../dao/Post"
 import CommentItem from "../dao/Comment"
-const hello = () => {
-    return "Hello World"
-}
 
-const posts = async () => {
-   const posts: PostItem[] = await Post.find()
+
+const POSTS_BY_PAGE = 10
+
+const posts = async (obj: any) => {
+    console.log("obj", obj)
+    console.log("page number:", obj.page = obj.page || 2)
+   const skip = (obj.page - 1) * POSTS_BY_PAGE
+
+   const posts: any = await Post.find()
+        .skip(skip)
+        .limit(POSTS_BY_PAGE)
+
    const newPosts = posts.map(async (post: PostItem) => {
     const filterComments: any = post.comments.map(async (comment: CommentItem) => {
         const commentItem = await Comment.findOne({id: comment})
@@ -18,8 +25,23 @@ const posts = async () => {
     post.comments = comments
     return post
    })
-   return await Promise.all(newPosts)
+   
+   const hasNextPage = posts.length === POSTS_BY_PAGE
+   const endCursor = hasNextPage ? posts[posts.length - 1].id : null
+
+   const resultPosts = await Promise.all(newPosts)
+
+   return {
+    edges: resultPosts.map((post: any) => ({
+        cursor: post.id,
+        node: post
+      })),
+      pageInfo: {
+        hasNextPage,
+        endCursor
+      }
+    } 
 }
 
-export default {hello, posts}
+export default { posts}
 
